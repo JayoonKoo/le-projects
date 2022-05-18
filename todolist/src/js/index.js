ce047d48c2fb2c7a80ce38d8790ca04a6398ba21
JayoonKoo <rnwkdbs12@gmail.com>
2022-05-18 23:00:08 +0900

    0518

--- todolist/src/js/index.js
+++ todolist/src/js/index.js
@@ -1,10 +1,90 @@
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import "../scss/style.scss";
 
+class Router {
+  routes = [];
+  notFondCallback = () => {};
+  addRoute(url, callback) {
+    this.routes.push({
+      url,
+      callback,
+    });
+
+    return this;
+  }
+
+  checkRoute() {
+    const currentRoute = this.routes.find(
+      (route) => route.url === window.location.hash
+    );
+
+    if (!currentRoute) {
+      this.notFondCallback();
+      return;
+    }
+
+    currentRoute.callback();
+  }
+
+  init() {
+    window.addEventListener("hashchange", this.checkRoute.bind(this));
+    if (!window.location.hash) {
+      window.location.hash = "#/";
+    }
+    this.checkRoute();
+  }
+
+  setNotFound(callback) {
+    this.notFondCallback = callback;
+    return this;
+  }
+}
+
+class Storage {
+  saveTodo(id, todoContent) {
+    const todosData = this.getTodos();
+    todosData.push({ id, content: todoContent, status: "TODO" });
+    localStorage.setItem("todos", JSON.stringify(todosData));
+  }
+
+  editTodo(id, todoContent, status = "TODO") {
+    const todosData = this.getTodos();
+    const todoIndex = todosData.findIndex((todo) => todo.id == id);
+    const targetTodo = todosData[todoIndex];
+    const editTodo =
+      todoContent === ""
+        ? { ...targetTodo, status }
+        : { ...targetTodo, content: todoContent };
+    todosData.splice(todoIndex, 1, editTodo);
+    localStorage.setItem("todos", JSON.stringify(todosData));
+  }
+
+  deleteTodo(id) {
+    const todosData = this.getTodos();
+    todosData.splice(
+      todosData.findIndex((todo) => todo.id == id),
+      1
+    );
+    localStorage.setItem("todos", JSON.stringify(todosData));
+  }
+
+  getTodos() {
+    return localStorage.getItem("todos") === null
+      ? []
+      : JSON.parse(localStorage.getItem("todos"));
+  }
+}
+
 class TodoList {
-  constructor() {
+  constructor(storage) {
+    this.initStorage(storage);
     this.assignElement();
     this.addEvent();
+    this.loadSaveData();
+  }
+
+  initStorage(storage) {
+    this.storage = storage;
   }
 
   assignElement() {
@@ -18,13 +98,27 @@ class TodoList {
     this.filterRadioEls = this.radioAreaEl.querySelectorAll(
       'input[name="filter"]'
     );
-    console.log(this.filterRadioEls);
   }
 
   addEvent() {
     this.addBtnEl.addEventListener("click", this.onClickAddBtn.bind(this));
     this.todoListEl.addEventListener("click", this.onClickTodoList.bind(this));
+    this.todoInputEl.addEventListener("keyup", this.onKeyUpTodo.bind(this));
     this.addRadioBtnEvent();
+  }
+
+  onKeyUpTodo(event) {
+    if (event.code === "Enter") {
+      this.onClickAddBtn();
+    }
+  }
+
+  loadSaveData() {
+    const todosData = this.storage.getTodos();
+    for (const todoData of todosData) {
+      const { id, content, status } = todoData;
+      this.createTodoElement(id, content, status);
+    }
   }
 
   addRadioBtnEvent() {
@@ -38,7 +132,7 @@ class TodoList {
 
   onClickRadioBtn(event) {
     const { value } = event.target;
-    this.filterTodo(value);
+    window.location.hash = `#/${value.toLowerCase()}`;
   }
 
   filterTodo(status) {
@@ -81,6 +175,12 @@ class TodoList {
   completeTodo(target) {
     const todoDiv = target.closest(".todo");
     todoDiv.classList.toggle("done");
+    const { id } = todoDiv.dataset;
+    this.storage.editTodo(
+      id,
+      "",
+      todoDiv.classList.contains("done") ? "DONE" : "TODO"
+    );
   }
 
   saveTodo(target) {
@@ -88,6 +188,8 @@ class TodoList {
     todoDiv.classList.remove("edit");
     const todoInputEl = todoDiv.querySelector("input");
     todoInputEl.readOnly = true;
+    const { id } = todoDiv.dataset;
+    this.storage.editTodo(id, todoInputEl.value);
   }
 
   editTodo(target) {
@@ -105,6 +207,7 @@ class TodoList {
       todoDiv.remove();
     });
     todoDiv.classList.add("delete");
+    this.storage.deleteTodo(todoDiv.dataset.id);
   }
 
   onClickAddBtn() {
@@ -114,13 +217,19 @@ class TodoList {
       return;
     }
 
-    this.createTodoElement(value);
+    const id = Date.now();
+    this.storage.saveTodo(id, value);
+    this.createTodoElement(id, value);
   }
 
-  createTodoElement(value) {
+  createTodoElement(id, value, status = null) {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+    if (status === "DONE") {
+      todoDiv.classList.add("done");
+    }
 
+    todoDiv.dataset.id = id;
     const todoContent = document.createElement("input");
     todoContent.value = value;
     todoContent.readOnly = true;
@@ -156,5 +265,18 @@ class TodoList {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  new TodoList();
+  const router = new Router();
+  const todoList = new TodoList(new Storage());
+  const routeCallback = (status) => () => {
+    todoList.filterTodo(status);
+    document.querySelector(
+      `input[type='radio'][value='${status}']`
+    ).checked = true;
+  };
+  router
+    .addRoute("#/all", routeCallback("ALL"))
+    .addRoute("#/todo", routeCallback("TODO"))
+    .addRoute("#/done", routeCallback("DONE"))
+    .setNotFound(routeCallback("ALL"))
+    .init();
 });
